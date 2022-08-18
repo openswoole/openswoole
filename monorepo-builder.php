5e6ae324862cb91d61eed91efd1a68df87e74c8b
Bruce Dou <doubaokun@gmail.com>
2022-08-18 16:30:48 +0100

    Fix ci

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -8,7 +8,6 @@ use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
     $parameters->set(Option::PACKAGE_DIRECTORIES, [
         __DIR__ . '/core',
         __DIR__ . '/grpc',
