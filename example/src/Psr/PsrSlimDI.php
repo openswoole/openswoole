0661b72b2110c296788e810aba8573232da7a1ac
Yifan Zhuo <im_zyf@outlook.com>
2022-11-01 18:47:20 +0800

    * DI example with Slim and Container

---
+++ example/src/Psr/PsrSlimDI.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+* This file is part of OpenSwoole.
+* @link     https://openswoole.com
+* @contact  hello@openswoole.com
+*/
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use OpenSwoole\HTTP\Server;
+use Slim\Factory\AppFactory;
+
+$container = new League\Container\Container();
+$container->add(AppFactory::class);
+$container->add(Server::class)->addArguments(['127.0.0.1', 9501]);
+
+$app = $container->get(AppFactory::class)->create();
+$app->get('/hello/{name}', function ($request, $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$server = $container->get(Server::class);
+$server->handle(function ($request) use ($app) {
+    return $app->handle($request);
+});
+$server->start();
