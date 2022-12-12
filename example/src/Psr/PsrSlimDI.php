fcc5547d8e885a49ac5dd0d9ad09821f220c9d8d
Bruce Dou <doubaokun@gmail.com>
2022-12-12 20:34:23 +0000

    Added di example

---
+++ example/src/Psr/PsrSlimDI.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of OpenSwoole.
+ * @link     https://openswoole.com
+ * @contact  hello@openswoole.com
+ */
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use OpenSwoole\HTTP\Server;
+use Slim\Factory\AppFactory;
+
+class HelloService {
+    public function hello($name) {
+        return "Hello, {$name}";
+    }
+}
+
+$container = new League\Container\Container();
+$container->add(HelloService::class);
+
+$app = AppFactory::create();
+$app->get('/hello/{name}', function ($request, $response, array $args) use ($container) {
+    $sevice = $container->get(HelloService::class);
+    $name = $args['name'];
+    $response->getBody()->write($sevice->hello($name));
+    return $response;
+});
+
+$server = new Server('127.0.0.1', 9501);
+$server->handle(function ($request) use ($app) {
+    return $app->handle($request);
+});
+$server->start();
